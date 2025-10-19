6638bc4eb140fd18b582ee3c297d6d8943ccf442
Raisibe25 <raisibemothiba25@gmail.com>
2025-10-19 14:16:42 +0200

    Fix formatting of password string in HashGenerator

--- src/main/java/com/app/tools/HashGenerator.java
+++ src/main/java/com/app/tools/HashGenerator.java
@@ -4,7 +4,7 @@ import com.app.security.PasswordUtil;
 
 public class HashGenerator {
     public static void main(String[] args) {
-        String password = "admin"; // choose your admin password
+        String password = "admin"; 
         String hash = PasswordUtil.hash(password);
         System.out.println("Bcrypt hash for admin: " + hash);
     }
